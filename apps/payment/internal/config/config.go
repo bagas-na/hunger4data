f4d5414da5eb93bdc4100cbcb92b362507a76576
bagas-na <105133641+bagas-na@users.noreply.github.com>
2026-01-30 15:54:13 +0700

    dev: create payment model and repo functions

--- apps/payment/internal/config/config.go
+++ apps/payment/internal/config/config.go
@@ -1,1 +1,42 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	HttpPort string
+	DBDSN    string
+}
+
+func Load() *Config {
+	env := os.Getenv("ENV")
+
+	// Default to production if ENV is not set
+	if env == "" {
+		env = "production"
+	}
+
+	// Use godotenv only in development
+	if env == "development" || env == "dev" {
+		if err := godotenv.Load(); err != nil {
+			fmt.Println("Failed to load .env file. Using existing and/or default ENV values")
+		}
+	}
+
+	return &Config{
+		HttpPort: getEnv("PORT", "9000"),
+		DBDSN:    getEnv("DB_DSN", "postgresql://postgres:postgres@localhost:5432/postgres"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		fmt.Printf("Missing env var: %s. Using fallback value: %s\n", key, fallback)
+	}
+	return val
+}
